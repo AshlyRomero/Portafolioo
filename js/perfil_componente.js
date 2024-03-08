8f6ae62175087ee7eb76ac62c0bef3acbb86b01a
AshlyRomero <126489849+AshlyRomero@users.noreply.github.com>
2024-03-08 08:44:19 -0600

    Add files via upload

---
+++ js/perfil_componente.js
@@ -0,0 +1,12 @@
+let perfil = `
+    <div class="nombre_usuario  ">
+        <div class="logo">
+            RM
+        </div>
+        <span>
+            AshlyVanessaRomero
+        </span>
+    </div>
+`;
+
+export {perfil}
